ee01523c71f18ffe7621d9ded695cdea011c1716
Olle <noemail>
2020-07-20 19:20:14 +0000

    Notes

--- _notes.php
+++ _notes.php
@@ -46,17 +46,53 @@ class InstallController
      */
     public function actionUpdateInstallation(int $installationId)
     {
-        $this->pipeline = [
+        $this->actions = [
             new Query('SELECT * FROM installations WHERE id = ' . $installationId),
-            new FilterNull(),
+            new FilterNull('Found no installation with this id'),
             new Filter(function($result) { return count($result === 1); }),
             new Filter(function($result) { return $result[0]->is_good_installation; }),
             new Query('UPDATE installations SET is_good_installation = 0 WHERE id = ' . $installationId),
             new FilterTrue('Could not update installation'),
             new Output('Success')
         ];
-        // TODO: If-statement action?
+        // TODO: If-statement or switch action?
         // TODO: How to unit-test? How to mock?
+        // TODO: Becomes a DSL?
+        // TODO: Hard to follow program flow?
+    }
+
+    public function updateUser(int $installationId)
+    {
+        return [
+            new Query('SELECT * FROM user WHERE id = ' . $installationId),
+            function ($result) {
+                if ($result->is_admin) {
+                    return [null, [new Query('UPDATE user SET is_admin = 0')]];
+                } else {
+                    return [null, [new Query('UPDATE user SET is_admin = 1')]];
+                }
+            },
+            function ($result) {
+                if ($result) {
+                    return [null, [new Output('Success')]];
+                } else {
+                    return [null, [new Output('Could not update user')]];
+                }
+            }
+        ];
+    }
+}
+
+class Pipeline
+{
+    public function run($result, $actions)
+    {
+        foreach ($actions as $action) {
+            $this->resolveDependencies($action);
+            list($result, $newActions) = $action($result);
+            $result = $this->run($result, $actions);
+        }
+        return $result;
     }
 }
 
